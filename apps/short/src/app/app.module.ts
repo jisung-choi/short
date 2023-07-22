82b64be8884cd367d768044186d1bd8b6e48e009
James Jisung Choi <70044510+jisung-choi@users.noreply.github.com>
2023-07-22 16:44:58 -0700

    Updates rxjs and resolves dependency

--- apps/short/src/app/app.module.ts
+++ apps/short/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { appRoutes } from './app.routes';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { AccordionModule } from 'primeng/accordion';
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+]
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, HomePageComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    ButtonModule,
+    InputTextModule,
+    CardModule,
+    ReactiveFormsModule,
+    AccordionModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent],
