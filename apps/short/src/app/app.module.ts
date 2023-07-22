23f0eeb4e64e9c511077d59c3243c7c5caeac6a2
James Jisung Choi <70044510+jisung-choi@users.noreply.github.com>
2023-07-21 20:27:07 -0700

    Project initialized, PrimeNg library set up, dependencies resolved

---
+++ apps/short/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent, HomePageComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
