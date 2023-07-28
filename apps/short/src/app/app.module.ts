1da9aeb170e3a50ebabecf9440c2a961ad7f1325
James Jisung Choi <70044510+jisung-choi@users.noreply.github.com>
2023-07-28 15:20:54 -0700

    minor fix

--- apps/short/src/app/app.module.ts
+++ apps/short/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 const routes: Routes = [
   {path: '', component: HomePageComponent},
   {
-    path: ':id',
+    path: '/:id',
     component: HomePageComponent
   },
 ]
